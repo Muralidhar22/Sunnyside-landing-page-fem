ddc498ab5122ffb1b4250d95f3db779d92432766
Muralidhar22 <muralidhar7794@gmail.com>
2022-03-21 18:47:58 +0530

    sections layout completed patch 1

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const menu = document.querySelector(".hamburger-menu__icon");
-const navMenu = document.querySelector(".nav-bar__mobile")
+const menu = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-bar")
 
 menu.addEventListener("click",displayMenu)
 
 
 function displayMenu() {
+  console.log(`clicked`)
   let navbar = getComputedStyle(navMenu)
   if(navbar.display === "none"){
     navMenu.classList.remove("dsply-none")
+    console.log(`display was none`)
   }
   else{
     navMenu.classList.add("dsply-none")
+    console.log(`display is set to none`)
   }
 }
